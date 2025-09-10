7e9fb65d2b12b7b80d76bf768e608bd7b66ce638
davis-bc <216034527+davis-bc@users.noreply.github.com>
2025-09-10 11:04:43 -0400

    rm errant slurm files from push

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2g
 #SBATCH --job-name=snakemake_driver
-#SBATCH --output=/work/NRSAAMR/Projects/SWAM/WGS/snakemake.%j
+#SBATCH --output=/work/NRSAAMR/Projects/SWAM/WGS/slurm/snakemake.%j
 
 snakemake --profile config/slurm/ --config in_dir="input/" out_dir="output/" --configfile config/config.yaml --use-conda --conda-frontend conda \
 	--group-components alpha=30 beta=2 -j 10
