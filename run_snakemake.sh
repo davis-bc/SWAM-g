62c29ee6d0c64c3bf86cd2409576602599b6ecde
davis-bc <216034527+davis-bc@users.noreply.github.com>
2025-09-15 16:08:41 -0400

    added seqsero2 for Salmonella serotyping

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -7,5 +7,9 @@
 #SBATCH --job-name=snakemake_driver
 #SBATCH --output=/work/NRSAAMR/Projects/SWAM/WGS/slurm/snakemake.%j
 
+cd /work/NRSAAMR/Projects/SWAM/WGS
+
 snakemake --profile config/slurm/ --config in_dir="input/" out_dir="output/" --configfile config/config.yaml --use-conda --conda-frontend conda \
-	--group-components alpha=30 beta=2 -j 10
+	  --group-components group1=30 group2=30 -j 30
+
+#snakemake --report report.html --config in_dir="input/" out_dir="output/" --configfile config/config.yaml
