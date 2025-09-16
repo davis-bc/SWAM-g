c04fa8b5878939a500b6cbd9819611c96b99c425
davis-bc <216034527+davis-bc@users.noreply.github.com>
2025-09-16 14:54:25 -0400

    functioning version w/ serotype splitting

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=nrsaamr
 #SBATCH --partition=ord
-#SBATCH --time=03:00:00
+#SBATCH --time=0-10:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2g
 #SBATCH --job-name=snakemake_driver
@@ -10,6 +10,6 @@
 cd /work/NRSAAMR/Projects/SWAM/WGS
 
 snakemake --profile config/slurm/ --config in_dir="input/" out_dir="output/" --configfile config/config.yaml --use-conda --conda-frontend conda \
-	  --group-components group1=30 group2=30 -j 30
+	  --group-components group1=100 group2=100 group3=100 group4=100 -j 10
 
 #snakemake --report report.html --config in_dir="input/" out_dir="output/" --configfile config/config.yaml
